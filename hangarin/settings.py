718f469805235ae6cd3a91e02aaf3a9c4f7eed97
janepaulineb <balofinosjanepauline@gmail.com>
2026-09-16 23:22:59 +0800

    modify allowed host

--- hangarin/settings.py
+++ hangarin/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2t$n9j#0ypbke-czt-625l)y1evpdnxe=)y$%x0a1@l#4^elg1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jane.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
